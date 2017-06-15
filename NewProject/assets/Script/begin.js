f5d04192401ef080cb1f5de44bd47e82e5ef64b5
zsn <925453683@qq.com>
2017-06-15 08:34:18 +0800

    begin

--- NewProject/assets/Script/begin.js
+++ NewProject/assets/Script/begin.js
@@ -2,27 +2,20 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //    default: null,      // The default value will be used only when the component attaching
-        //                           to a node for the first time
-        //    url: cc.Texture2D,  // optional, default is typeof default
-        //    serializable: true, // optional, default is true
-        //    visible: true,      // optional, default is true
-        //    displayName: 'Foo', // optional
-        //    readonly: false,    // optional, default is false
-        // },
-        // ...
+        storyMenu:cc.Node.Node,
     },
 
     // use this for initialization
     onLoad: function () {
-        var timeIn = 1;
-        this.schedule(function(){
-            timeIn--;
-            if(timeIn===0){
-                cc.director.loadScene('main');
-            }
-        },1);
+
+    },
+
+    onBtnStory: function(){
+        this.storyMenu = true;
+    },
+
+    onBtnPlay: function(){
+        cc.director.loadScene("load");
     },
 
     // called every frame, uncomment this function to activate update callback
